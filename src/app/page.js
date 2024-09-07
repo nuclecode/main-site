02ef39400d71b0ebb9c16cf2921a2b386894cf8d
idncod <idncod@protonmail.com>
2024-09-07 22:59:17 +0100

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>src/app/page.js</code>.
+            This is a test nuclecode project.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
